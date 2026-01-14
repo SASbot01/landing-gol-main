6cc6ddc57d9c3b4c6edc9f8fedfdf3053e9b0e5e
SASbot01 <silvestrefuentesalejandro@gmail.com>
2026-01-14 20:45:28 +0100

    Update Stripe Founders payment link to production URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -11,7 +11,7 @@ export const AUTH_URL = 'https://gol.blackwolfsec.io/auth';
 // Stripe Payment Links (Production)
 export const STRIPE_MONTHLY_LINK = 'https://buy.stripe.com/7sY3cu5aratnexD1eM5kk01'; // $37/month - INITIATE
 export const STRIPE_LIFETIME_LINK = 'https://buy.stripe.com/14A28qauL6d789f3mU5kk02'; // $333 lifetime - SOVEREIGN
-export const STRIPE_FOUNDERS_LINK = '#'; // $97 lifetime - FOUNDERS (Placeholder)
+export const STRIPE_FOUNDERS_LINK = 'https://buy.stripe.com/9B6bJ08mD5933SZ6z65kk03'; // $97 lifetime - FOUNDERS
 
 export const ROUTES = {
     HOME: '/',
